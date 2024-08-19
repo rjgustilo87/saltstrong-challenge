2b273640e8ef73c3664a9fc66cf7158b5fc343e3
RJ Gustilo <rjgustilo@RJs-MacBook-Pro.local>
2024-08-19 11:52:07 -0700

    Initial commit with project setup

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].[hash].js`,
+        chunkFileNames: `[name].[hash].js`,
+        assetFileNames: `[name].[hash][extname]`
+      }
+    }
+  }
+});
